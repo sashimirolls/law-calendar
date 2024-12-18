e552716f997c4eb88e5bb59c752c2b60968ddabd
sashimirolls <119821932+sashimirolls@users.noreply.github.com>
2024-12-18 02:09:23 -0800

    Update src/services/config.ts [BoltSync]

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,7 +1,9 @@
 export const API_CONFIG = {
-  BASE_URL: 'https://law-calendar.vercel.app/api',
+  BASE_URL: import.meta.env.PROD 
+    ? 'https://law-calendar.vercel.app/api'
+    : '/api',
   ACUITY: {
-    BASE_URL: 'https://acuityscheduling.com/api/v1/availability',
+    BASE_URL: 'https://acuityscheduling.com/api/v1',
     API_KEY: import.meta.env.ACUITY_API_KEY,
     USER_ID: import.meta.env.ACUITY_USER_ID,
     APPOINTMENT_TYPE: import.meta.env.APPOINTMENT_TYPE
