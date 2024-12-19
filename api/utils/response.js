313c026f6412069e2b5643924a7d14feb6447349
sashimirolls <119821932+sashimirolls@users.noreply.github.com>
2024-12-18 19:44:54 -0800

    Update api/utils/response.js [BoltSync]

---
+++ api/utils/response.js
@@ -0,0 +1,20 @@
+// Common CORS headers
+export const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization, Accept',
+  'Access-Control-Max-Age': '86400',
+  'Cache-Control': 'no-cache'
+};
+
+// Helper to send JSON response with CORS headers
+export function sendResponse(res, statusCode, data) {
+  return res.status(statusCode)
+    .setHeader('Content-Type', 'application/json')
+    .setHeader('Access-Control-Allow-Origin', '*')
+    .setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+    .setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept')
+    .setHeader('Access-Control-Max-Age', '86400')
+    .setHeader('Cache-Control', 'no-cache')
+    .json(data);
+}
