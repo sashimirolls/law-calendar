e4a6ad5e00ec9cbc8926cf5389fa3604d41c5e4c
sashimirolls <119821932+sashimirolls@users.noreply.github.com>
2024-12-18 20:29:10 -0800

    Update vite.config.ts [BoltSync]

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  envPrefix: ['ACUITY_', 'API_'],
+  envPrefix: ['VITE_', 'ACUITY_', 'API_'],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
