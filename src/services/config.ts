623162abc00488714404c767dcef890739409202
sashimirolls <119821932+sashimirolls@users.noreply.github.com>
2024-12-18 20:46:01 -0800

    Update src/services/config.ts [BoltSync]

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,9 +1,11 @@
 export const API_CONFIG = {
   BASE_URL: import.meta.env.PROD 
-    ? '/.netlify/functions'
+    ? 'https://law-calendar.vercel.app/api'
     : '/api',
   ACUITY: {
-    BASE_URL: '/.netlify/functions',
+    BASE_URL: import.meta.env.PROD 
+      ? 'https://law-calendar.vercel.app/api'
+      : '/api',
     API_KEY: import.meta.env.ACUITY_API_KEY,
     USER_ID: import.meta.env.ACUITY_USER_ID,
     APPOINTMENT_TYPE: import.meta.env.APPOINTMENT_TYPE
