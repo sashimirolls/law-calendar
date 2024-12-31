9e482a6219a57b29798b13bc22d64086e7057be9
sashimirolls <119821932+sashimirolls@users.noreply.github.com>
2025-01-01 04:30:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,9 @@ import availabilityRoutes from './server/routes/availability.js';
 const app = express();
 
 app.use(cors({
-    origin: 'https://chimerical-eclair-1c93b2.netlify.app', 
-    methods: ['GET', 'POST', 'PUT', 'PATCH'],
+    origin: 'https://chimerical-eclair-1c93b2.netlify.app', // Replace with your frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
 }));
 app.use(express.json());
 app.use(loggingMiddleware);
