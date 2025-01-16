9665838b5be43763d55307237a8e5ec2583d547d
sashimirolls <119821932+sashimirolls@users.noreply.github.com>
2025-01-16 18:44:10 +0530

    Delete backend directory

--- backend/server/middleware/logging.js
+++
@@ -1,5 +0,0 @@
-export function loggingMiddleware(req, res, next) {
-  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
-  console.log('Query params:', req.query);
-  next();
-}
